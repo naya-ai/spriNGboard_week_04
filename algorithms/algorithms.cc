1dd6f0c9916875d00d83bc107fa65a9ee1c3e78c
shawnya <shawnaya@workspace.com>
2023-02-19 20:00:14 -0600

    added code

--- algorithms/algorithms.cc
+++ algorithms/algorithms.cc
@@ -12,45 +12,23 @@ void print_vector (const vector<int>& v);
 
 void test_algorithms (int N)
 {
-  //////////////////////////////
-  // CREATE A VECTOR v OF int //
-  // WITH SIZE N              //
-  // FILLED WITH ZEROS        //
-  //////////////////////////////
-
+  vector<int> v(N,0);
 
   print_vector(v);
 
-  ///////////////////////////////
-  // USE iota TO FILL v        //
-  // WITH VALUES STARTING AT 5 //
-  ///////////////////////////////
-
+  iota(v.begin(), v.end(), 5);
 
   print_vector(v);
 
-  ///////////////////////////////////////
-  // USE reverse TO REVERSE THE VALUES //
-  // BETWEEN THE THIRD AND FIFTH FROM  //
-  // LAST POSITIONS                    //
-  ///////////////////////////////////////
-
+  reverse(v.begin()+2, v.end()-4);
 
   print_vector(v);
 
-  /////////////////////////////////////
-  // USE fill TO FILL THE FIRST FOUR //
-  // VALUES WITH 20s                 //
-  /////////////////////////////////////
-
+  fill(v.begin(), v.end()-11, 20);
 
   print_vector(v);
 
-  ////////////////////////////////////////
-  // USE sort TO SORT ALL BUT THE FIRST //
-  // AND LAST VALUES                    //
-  ////////////////////////////////////////
-
+   sort (v.begin()+1, v.end()-1);
 
   print_vector(v);
 
@@ -60,6 +38,16 @@ void test_algorithms (int N)
   // FRONT AND ALL ODD NUMBERS TO THE BACK //
   ///////////////////////////////////////////
 
+sort(v.begin(), v.end(), [](const int& front, const int& back) -> bool
+  {
+   if(front & 1 && back & 1)  
+ return false;
+ else if(front & 1)
+ return false;
+  else if(back & 1)
+   return true; 
+  return false; 
+     });
 
   print_vector(v);
 }
